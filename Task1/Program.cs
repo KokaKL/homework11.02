dd4bad75d8ab17405f9d67d3ae4f27c222dfdf2b
Koka <kshurgaya91@gmail.com>
2023-02-12 03:08:40 +0300

    task1

---
+++ Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+    Console.WriteLine();
+}
+int InputInt(string message)
+{
+    System.Console.WriteLine(message + ">");
+    string inputedValie = Console.ReadLine();
+    int result = Convert.ToInt32(inputedValie);
+    return result;
+}
+int n = InputInt("Введите количество чисел: ");
+
+int[] array = new int[n];
+PrintArray(array);
+
+for (int i = 0; i < n; i++)
+    {
+        System.Console.WriteLine($"Введите число: ");
+        string I =  Console.ReadLine();
+        array[i] = Convert.ToInt32(I);
+    }
+
+PrintArray(array);
+
